0a61213bb98aa3ab0daf991329d5bcdb3680d823
Stefan Penner <stefan.penner@gmail.com>
2016-09-20 15:11:53 -0700

    bump dependencies

--- tests/index.js
+++ tests/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var RSVP = require('rsvp');
 var expect = require('expect.js');
 var walkSync = require('walk-sync');
-var broccoli = require('broccoli');
+var broccoli = require('broccoli-builder');
 var rimraf = RSVP.denodeify(require('rimraf'));
 
 require('mocha-jshint')();
