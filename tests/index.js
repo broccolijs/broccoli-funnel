bfa50882ed0eb40e2a2fefc9c95a7cbc05614c80
Robert Jackson <rjackson@linkedin.com>
2021-04-15 16:46:49 -0400

    Fix test to properly await for the build.

--- tests/index.js
+++ tests/index.js
@@ -180,14 +180,16 @@ describe('broccoli-funnel', function() {
       expect(assertions).to.equal(0, 'Build did not throw an error, relative path traversal worked.');
     });
 
-    it('missing srcDir with allowEmpty results in empty destDir', async function() {
+    it("missing srcDir with allowEmpty results in empty destDir", async function() {
       let node = new Funnel(FIXTURE_INPUT, {
         allowEmpty: true,
-        srcDir: 'i-dont-exist',
-        destDir: 'my-output',
+        srcDir: "i-dont-exist",
+        destDir: "my-output",
       });
       output = createBuilder(node);
-      expect(walkSync(output.path())).to.eql(['my-output/']);
+      await output.build();
+
+      expect(walkSync(output.path())).to.eql(["my-output/"]);
     });
 
     it('throws error on unspecified allowEmpty', async function() {
